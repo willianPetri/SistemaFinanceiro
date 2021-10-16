c1d908ac4a33e2b65994948beef9aefed780feb6
willian <wopetri11@gmail.com>
2021-10-15 22:22:20 -0400

    Updated App and Added Items

---
+++ src/data/items.ts
@@ -0,0 +1,6 @@
+ export const items = [
+   { date: new Date(2021, 9, 15), category: 'food', tilte: 'McDonalds', value: 32.12 },
+   { date: new Date(2021, 9, 15), category: 'food', tilte: 'Burguer King', value: 30 },
+   { date: new Date(2021, 9, 16), category: 'rent', tilte: 'Aluguel', value: 2000 },
+   { date: new Date(2021, 9, 18), category: 'salary', tilte: 'Salário', value: 5300 },
+ ]
