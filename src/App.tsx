e34aa2cf46d7d848a1104f4e098a328515b3dcc3
willian <wopetri11@gmail.com>
2021-10-17 12:58:22 -0400

    Added InfoArea

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,23 @@ import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilters';
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
+
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return(
     <C.Container>
@@ -23,7 +31,12 @@ const App = () => {
       </C.Header>
       <C.Body>
         
-        
+        <InfoArea 
+          currentMonth={currentMonth} 
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
 
         <TableArea list={filteredList} />
 
