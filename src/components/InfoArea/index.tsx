f07b3760d13291fc56a5ed4e378df47c2f498244
willian <wopetri11@gmail.com>
2021-10-17 13:28:56 -0400

    Added InputArea and ResumeItem. Updated App

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,5 +1,6 @@
 import * as C from './styles';
 import { formatCurrentMonth } from '../../helpers/dateFilters';
+import { ResumeItem } from '../ResumeItem';
 
 
 type Props = {
@@ -33,7 +34,13 @@ export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props
         <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
       </C.MonthArea>
       <C.ResumeArea>
-        
+        <ResumeItem title="Receitas" value={income} />
+        <ResumeItem title="Despesas" value={expense} />
+        <ResumeItem 
+          title="Balanço" 
+          value={income - expense} 
+          color={(income - expense) < 0 ? 'red' : 'green'}
+        />
       </C.ResumeArea>
     </C.Container>
   );
